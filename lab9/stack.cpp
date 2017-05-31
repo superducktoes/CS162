1b983e597d18f51719edf0d00cfd80893c95ce9f
superducktoes <superducktoes@gmail.com>
2017-05-31 01:12:27 -0400

    removed commented out main

--- lab9/stack.cpp
+++ lab9/stack.cpp
@@ -2,8 +2,8 @@
  *
  * Author: Nick Roy
  * Date: 6/4/2017
- * Description: Palindrome stack. Loops through the word entered by the 
- * user and pushes on the stack. Second loop pops off the stack in 
+ * Description: Palindrome stack. Loops through the word entered by the
+ * user and pushes on the stack. Second loop pops off the stack in
  * order to generate the palindrome.
  *
  ********************************************************************/
@@ -21,30 +21,17 @@ using std::string;
 void palindromeStack(string p) {
   string palindrome = p;
   std::stack<char> myPalStack;
-  
+
   for(int i=0; i<palindrome.length(); i++) {
     myPalStack.push(palindrome.at(i));
   }
-  
+
   cout << "The word you entered is: " << palindrome << endl;
   cout << "\nThe palindrome of the word is: ";
-  
+
   while(!myPalStack.empty()) {
     cout << myPalStack.top();
     myPalStack.pop();
   }
   cout << endl;
 }
-
-/*
-int main() {
-
-  string userWord;
-  
-  cout << "Enter a word to make a palindrome out of: ";
-  getline(cin,userWord);
-  palindromeStack(userWord);
-  
-  return 0;
-}
-*/
